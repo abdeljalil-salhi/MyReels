26029c3a9542966bac190cf51174f30f23a08887
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-03-06 17:49:35 +0100

    INITIAL PUSH: Commit to github

---
+++ client/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA2J2BBMwcRkVqqDiK6OJSV_kxy57zNxSo",
+  authDomain: "myreels-61ee8.firebaseapp.com",
+  projectId: "myreels-61ee8",
+  storageBucket: "myreels-61ee8.appspot.com",
+  messagingSenderId: "303747165614",
+  appId: "1:303747165614:web:0332a56f766c0ea1bfec3f",
+  measurementId: "G-2V9WL7BR2T",
+};
